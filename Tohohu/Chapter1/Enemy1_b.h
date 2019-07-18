515ea9523cf2e0f33e9c0c595767427097e7bd09
tekito <tekito@kk.kk>
2019-07-18 16:09:03 +0900

    敵初期情報の構造体追加

--- Tohohu/Chapter1/Enemy1_b.h
+++ Tohohu/Chapter1/Enemy1_b.h
@@ -1,8 +1,19 @@
 #pragma once
+
 #define ENEMY1_B_SIZE_X 32		// 敵1_BのｻｲｽﾞX
 #define ENEMY1_B_SIZE_Y 32		// 敵1_BのｻｲｽﾞY
 #define ENEMY1_B_NUM	50		// 敵1_Bの配列個数
 #define ENEMY1_B_SPEED	4.0f	// 敵1_Bの移動量
+
+// 構造体定義
+// 敵の配置情報
+struct EnemyInit
+{
+	int count;		// 出現するﾌﾚｰﾑ
+	int movePtn;	// 移動ﾊﾟﾀｰﾝ
+	Vector2 pos;	// 初期座標
+	int moveAng;	// 移動角度
+};
 
 // extern宣言
 #ifdef ENEMY1_B_DEF
